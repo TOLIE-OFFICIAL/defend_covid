762520abd71dd632554ee307e186ece866751c85
Sinji-918 <81312918+Sinji-918@users.noreply.github.com>
2022-07-24 23:47:08 +0800

    Update index.vue

--- src/components/Covid/index.vue
+++ src/components/Covid/index.vue
@@ -1,8 +1,72 @@
 <!-- covid页面的内部主要组件 数据展示-->
 <template>
-  <h1>数据展示页面</h1>
+  <div class="app-container">
+    <div class="left">
+      <div class="left_top">左侧总体数据</div>
+      <div class="left_bottom">左侧占比数据</div>
+    </div>
+    <div class="center">
+      <div class="center_top">总体数据</div>
+      <ChinaMapVue />
+    </div>
+
+    <div class="right">
+      <Rank_ChartVue />
+      <div class="right_bottom">右侧各大洲数据</div>
+    </div>
+  </div>
 </template>
 
 <script>
-export default {}
+import ChinaMapVue from './ChinaMap.vue'
+import Rank_ChartVue from './rank_Chart.vue'
+import proportion_ChartVue from './proportion_Chart.vue'
+import trend_ChartVue from './trend_Chart.vue'
+export default {
+  components: {
+    ChinaMapVue,
+    Rank_ChartVue,
+    proportion_ChartVue,
+    trend_ChartVue
+  }
+}
 </script>
+<style scoped lang="scss">
+.app-container {
+  display: flex;
+  height: 790px;
+  .center {
+    flex: 1 1 50%;
+    width: 50%;
+    .center_top {
+      height: 30%;
+      background-color: burlywood; //添加组件时记得删去
+    }
+  }
+  .left {
+    .left_top {
+      width: 100%;
+    }
+    .left_top,
+    .left_bottom {
+      height: 50%;
+      background-color: aqua; //添加组件时记得删去
+    }
+  }
+  .right {
+    .right_top {
+      width: 100%;
+    }
+    .right_top,
+    .right_bottom {
+      height: 50%;
+      background-color: bisque; //添加组件时记得删去
+    }
+  }
+  .left,
+  .right {
+    width: 25%;
+    flex: 1 1 25%;
+  }
+}
+</style>
